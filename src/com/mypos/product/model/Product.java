d59238766e169dcae0619b16097192f3809a02c3
HEXCON99 <kristiantowidhi@gmail.com>
2025-12-09 17:11:21 +0700

    widhiii

--- src/com/mypos/product/model/Product.java
+++ src/com/mypos/product/model/Product.java
@@ -1,8 +1,35 @@
 package com.mypos.product.model;
 
-/**
- * A simple data class representing a product in the inventory.
- */
+package com.mypos.cashier.model;
+
 public class Product {
-    // TODO: Add fields for product ID, name, price, and stock quantity.
+
+    private String idProduct;   // barcode / id product
+    private String name;
+    private double price;
+
+    public String getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(String idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
+
